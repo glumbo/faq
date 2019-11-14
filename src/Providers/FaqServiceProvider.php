cb42e1aedf1fadf40073b3ba82adab136e45623e
glumbo <aliihsanarslan@yahoo.com>
2019-11-14 10:35:55 +0300

    Creating provider

---
+++ src/Providers/FaqServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Glumbo\Faq\Providers;
+use Illuminate\Support\ServiceProvider;
+
+Class FaqServiceProvider extends ServiceProvider{
+
+    public function boot(){
+       
+
+    }
+
+    public function regsiter(){
+
+
+    }
+}
+?>
