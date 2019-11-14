1e7961a11f89934c23c871aabf4361ecd6bb21de
glumbo <aliihsanarslan@yahoo.com>
2019-11-14 16:26:10 +0300

    publishing

--- src/Providers/FaqServiceProvider.php
+++ src/Providers/FaqServiceProvider.php
@@ -9,11 +9,53 @@ Class FaqServiceProvider extends ServiceProvider{
        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
        $this->loadViewsFrom(__DIR__.'/../resources/views', 'faq');
 
+        $this->publishConfig();
+        $this->publishMigrations();
+
     }
 
-    public function regsiter(){
+    public function register()
+    {
+        $this->mergeConfig();
+    }
 
+    private function mergeConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->mergeConfigFrom($path, 'faq');
+    }
 
+    private function publishConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->publishes([$path => config_path('faq.php')], 'config');
+    }
+
+    private function publishMigrations()
+    {
+        $path = $this->getMigrationsPath();
+        $this->publishes([$path => database_path('migrations')], 'migrations');
+    }
+
+    private function publishViews()
+    {
+        $path = $this->getViewsPath();
+        $this->publishes([$path => resource_path('views')], 'views');
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../config/faq.php';
+    }
+
+    private function getMigrationsPath()
+    {
+        return __DIR__ . '/../database/migrations/';
+    }
+
+    private function getViewsPath()
+    {
+        return __DIR__ . '/../resource/views/';
     }
 }
 ?>
