52518167b6c6b527be1ec7118a8d71697d0f1a06
glumbo <aliihsanarslan@yahoo.com>
2019-11-14 17:11:29 +0300

    publishing views and migrations

--- src/Providers/FaqServiceProvider.php
+++ src/Providers/FaqServiceProvider.php
@@ -9,6 +9,9 @@ Class FaqServiceProvider extends ServiceProvider{
        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
        $this->loadViewsFrom(__DIR__.'/../resources/views', 'faq');
 
+       $this->publishes([__DIR__.'/../database/migrations/' => database_path('migrations')], 'faq-migrations');
+       $this->publishes([__DIR__.'/../resources/views' => resource_path('views/vendor/faqs')], 'faq-views');
+
     }
 
     public function regsiter(){
